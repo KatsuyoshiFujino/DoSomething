04f8ea9582929cf5ad65339ba198b67e26838336
WS-PC08\ws1 <k_fujino@workscience.jp>
2017-01-23 10:35:56 +0900

    first commit

---
+++ Assets/Script/StageController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StageController : MonoBehaviour {
+
+	public GameObject prevStage;
+	public GameObject Stage;
+	public GameObject nextStage;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void MoveToNextStage(){
+		Destroy(Stage);
+		Instantiate(nextStage);
+	}
+
+}
